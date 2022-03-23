8b0fb0d0e9e2f66e3a1bb7ab93b1721b197e4f18
Viktoria <zaharova181114@gmail.com>
2022-03-23 12:09:50 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,72 @@
+$('.home-slider').slick({
+    slidesToShow: 1,
+    arrows: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
+$('.advantages-slider').slick({
+    slidesToShow: 6,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                variableWidth: true,
+                arrows: false
+            }
+        }
+    ]
+});
+
+$('.btn-burger').on('click', function () {
+   $('.fixed-block').fadeToggle();
+});
+
+//Сменим язык календаря на русский
+$.datepicker.setDefaults(
+    {
+        closeText: 'Закрыть',
+        prevText: '',
+        currentText: 'Сегодня',
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+            'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+            'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        weekHeader: 'Не',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    });
+
+$(function () {
+    $(".datepicker").datepicker();
+});
+
+$(document).scroll(function () {
+    if ($(this).scrollTop() <= ($(".home").offset().top - $(window).height())) {
+        $('.fixed-block-wrapper').css('opacity', 0);
+    } else {
+        $('.fixed-block-wrapper').css('opacity', 1);
+    }
+});
