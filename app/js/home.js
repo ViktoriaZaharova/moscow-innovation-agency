8f8599e8b1f21e06663a73046bba10006cbe0d26
Viktoria <zaharova181114@gmail.com>
2022-03-26 20:11:09 +0200

    edit

--- app/js/home.js
+++ app/js/home.js
@@ -1,7 +1,15 @@
-$(document).scroll(function () {
-    if ($(this).scrollTop() <= ($(".home").offset().top - $(window).height())) {
-        $('.fixed-block-wrapper').css('opacity', 0);
-    } else {
-        $('.fixed-block-wrapper').css('opacity', 1);
-    }
+$(function () {
+    var topPos = $('.fixed-block').offset().top;
+    $(window).scroll(function () {
+        var top = $(document).scrollTop(),
+            pip = $('footer').offset().top,
+            height = $('.fixed-block').outerHeight();
+        if (top > topPos && top < pip - height) {
+            $('.fixed-block').addClass('fixed').removeAttr("style");
+        } else if (top > pip - height) {
+            $('.fixed-block').addClass('fixed').removeAttr("style");
+        } else {
+            $('.fixed-block').removeClass('fixed');
+        }
+    });
 });
